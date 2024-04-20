7bf930d4a52305f6a1071d582defc2863bf7c7f8
Emilio <emiliocantil726@gmail.com>
2024-04-20 17:09:12 -0300

    exercio JSX01

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from "react"
+import Exercicio from './aula02Jsx.jsx'
+
 
 const App = () => {
-  const random = Math.random().toFixed(2)
-  const style = {
-   color: "blue", 
-   width: "100%",
-   height: "70px",
-   borderRadius: "2%",
-   background: "red",
-  }
-
-  const title = () =>{
-    return <p>Is the tittle for Origamid</p>
-  }
-
-  const isActive = true;
   return (
     <React.Fragment>
-      <h1>{title()}</h1>
-      <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome" />
-      <p style={style} className={isActive == true ? "active" : "noActive" }>{random}</p>
-      <button>Remove</button>
+      <Exercicio/>
     </React.Fragment>
   )
 }
