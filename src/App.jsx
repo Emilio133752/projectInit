e2ea1c3afe997574ed9b9086af9a4b29416e4629
Emilio <emiliocantil726@gmail.com>
2024-04-20 15:43:56 -0300

    alterações confirmadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const App = () => {
   return (
-    <button><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum expedita adipisci, ab minima sed non, impedit fugiat fugit quasi dolore nam labore.</p></button>
+    <button><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum expedita adipisci, asdfasdfasdfasdfasdfab minima sed non, impedit fugiat fugit quasi dolore nam labore.</p></button>
   )
 }
 
