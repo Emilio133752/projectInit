a00d2c860c5c1ba9b5b96ab6667405e6ac37bcb9
Emilio <emiliocantil726@gmail.com>
2024-04-20 16:15:55 -0300

    usando estilos em react objetos funções e outros

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react"
 
 const App = () => {
+  const random = Math.random().toFixed(2)
+  const style = {
+   color: "blue", 
+   width: "100%",
+   height: "70px",
+   borderRadius: "2%",
+   background: "red",
+  }
+  const isActive = true;
   return (
     <React.Fragment>
       <label htmlFor="nome">Nome</label>
       <input type="text" id="nome" />
+      <p style={style} className={isActive == true ? "active" : "noActive" }>{random}</p>
       <button>Remove</button>
     </React.Fragment>
   )
