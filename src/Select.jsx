e905f2fbb50c5b9d415b91c04c4b49bdafba5635
Emilio <emiliocantil726@gmail.com>
2024-05-12 15:02:15 -0300

    exercio com checked

--- src/Select.jsx
+++ src/Select.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
+
 const Select = () => {
-    const [termos, setTermos] = React.useState(false);
-  return (
-    <form>
-        <label>
-            <input type="checkbox" value='termo accept' checked={termos}onChange={({target}) => setTermos(target.checked)}/>
-            Aceito os termos
-        </label>
-        {termos && <p>Aceitou os termos</p>}
-    </form>
+    const [cores, setCores] = React.useState(['verde']);
+
+    function handleChange({ target }) {
+      if (target.checked) {
+        setCores([...cores, target.value]);
+      } else {
+        setCores(cores.filter((cor) => cor !== target.value));
+      }
+    } 
+
+    return (
+      <form>
+        {
+            coresArray.map((valores, index) => 
+            <label key={index}>
+                <input 
+                type="checkbox"
+                value={valores}
+                checked={cores.includes(valores)}
+                onChange={handleChange}
+                />
+                {valores}
+            </label>
+            )
+        }
+        <ul>
+          {cores.map((cor) => (
+            <li key={cor}>{cor}</li>
+          ))}
+        </ul>
+      </form>
   )
 }
 
